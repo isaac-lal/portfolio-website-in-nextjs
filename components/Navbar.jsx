5b3a6430c10de99380655f4032d6beb7a1c3e18a
Isaac Lal <isaaclal124@gmail.com>
2023-01-30 09:49:41 -0500

    29:07

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
@@ -24,12 +25,31 @@ const Navbar = () => {
               <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Projects</li>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
             </Link>
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
             </Link>
           </ul>
+
+          <div className="md:hidden">
+            <AiOutlineMenu size={25}/>
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <Image src='/../public/assets/navLogo.png' width='87' height='35' alt='/'/>
+            <div>
+              <AiOutlineClose />
+            </div>
+          </div>
         </div>
       </div>
     </div>
