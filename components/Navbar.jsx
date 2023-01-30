2fcb6e7cb99c5435b10864ea2a0a006a131941ea
Isaac Lal <isaaclal124@gmail.com>
2023-01-30 09:52:41 -0500

    30:09

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
 
       <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
         <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
-          <div>
+          <div className='flex w-full items-center justify-between'>
             <Image src='/../public/assets/navLogo.png' width='87' height='35' alt='/'/>
             <div>
               <AiOutlineClose />
